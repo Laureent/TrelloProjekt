962da46012682219be4a076beab5f931806bf509
Anteesz <varianti.antika@gmail.com>
2022-02-01 14:46:44 +0100

    jsdoc_eleje

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ let token = document.getElementById('token').value;
 let apikey = "6e9161711b4b1a09d3617140440da447";
 let tables = Array();
 
+
+/**
+ * Bekéri a táblázatot az api kulcs és token alapján
+ */
 async function getTables(){   
     let response = await fetch("https://api.trello.com/1/members/me/boards?key="+apikey+"&token="+token+"");
     let data = await response.json();
