9ed7d932c4c48d0b085f4bc901c5f18be874034d
Laurent <laurent@email.com>
2022-02-01 14:19:14 +0100

    lekérdezés

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let token = document.getElementById('token').value;
+let apikey = "6e9161711b4b1a09d3617140440da447";
+let tables = Array();
+
+async function getTables(){   
+    let response = await fetch("https://api.trello.com/1/members/me/boards?key="+apikey+"&token="+token+"");
+    let data = await response.json();
+    tables = data;
+    showTables();
+    document.getElementById('maketable').style.display = "none";
+}
+
+function showTables(){
+    console.log(tables);
+}
+
+
