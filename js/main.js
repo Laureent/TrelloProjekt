ed93526dd915c355d00bf75bff1dfd75d1ecb837
Lorey <loka9889@gmail.com>
2022-02-06 14:49:48 +0100

    folytatás de nem lehet mert hulladék az egész trello api

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
 let token = document.getElementById('token').value;
 let apikey =document.getElementById('apikey').value;
 let tables = Array();
-
+let cards = Array();
 
 /**
  * Bekéri a táblázatot az api kulcs és token alapján
  */
 async function getTables(){   
-    let response = await fetch("https://api.trello.com/1/members/me/boards?key="+apikey+"&token="+token+"");
+    //let response = await fetch("https://api.trello.com/1/members/me/boards?key="+apikey+"&token="+token+"");
+    let response = await fetch("https://api.trello.com/1/members/me/boards?key=7d05d50b5b8a4569bf25096ffbd2a384&token=bf48aa27fb42789832620a3c57ae6c8025c18bb94d7be86f11ce732f07c7dfa3");
     let data = await response.json();
     tables = data;
     showTables();
     document.getElementById('maketable').style.display = "none";
+    console.log(tables)
 }
+
 
 function showTables(){
     tables.forEach(element => {
         let button = document.createElement('button');
         button.innerText = element.name;
         button.className = "col tableButton";
-        button.onclick = function() { displayTable(element.name); }
+        button.onclick = function() { displayTable(element.id); }
         document.getElementById('main').append(button);
     })
 }
 
-function displayTable(table){
+function displayTable(tableid){
     document.getElementById('main').style.display = "none";
+    getCards(tableid)
 }
+
+// async function getCards(tableid){
+//     let response = await fetch("https://api.trello.com/1/cards?key=7d05d50b5b8a4569bf25096ffbd2a384&token=bf48aa27fb42789832620a3c57ae6c8025c18bb94d7be86f11ce732f07c7dfa3");
+// }
 
 
