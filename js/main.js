0c814c4cf0ce5e2a204e3da72787e2c4257afa20
Laurent <laurent@email.com>
2022-02-08 10:40:20 +0100

    lists/cards

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 let token = document.getElementById('token').value;
-let apikey =document.getElementById('apikey').value;
+let apikey = document.getElementById('apikey').value;
 let tables = Array();
 let cards = Array();
+let lists = Array();
+
 
 /**
  * Bekéri a táblázatot az api kulcs és token alapján
  */
 async function getTables(){   
-    //let response = await fetch("https://api.trello.com/1/members/me/boards?key="+apikey+"&token="+token+"");
-    let response = await fetch("https://api.trello.com/1/members/me/boards?key=7d05d50b5b8a4569bf25096ffbd2a384&token=bf48aa27fb42789832620a3c57ae6c8025c18bb94d7be86f11ce732f07c7dfa3");
+    let response = await fetch("https://api.trello.com/1/members/me/boards?key="+apikey.trim()+"&token="+token.trim());
     let data = await response.json();
     tables = data;
     showTables();
     document.getElementById('maketable').style.display = "none";
     console.log(tables)
 }
-
 
 function showTables(){
     tables.forEach(element => {
@@ -29,11 +29,35 @@ function showTables(){
 
 function displayTable(tableid){
     document.getElementById('main').style.display = "none";
-    getCards(tableid)
+    getCards(tableid);
+    getLists(tableid);
+    let template = document.getElementsByTagName('template')[0];
+    lists.forEach(element=>{
+        let list = document.importNode(template,true);
+        list.querySelector('h5').innerHTML = element.name;
+        cards.forEach(card => {
+            if(card.idList == element.id){
+                let p = document.createElement('p');
+                p.innerHTML = card.name;
+                list.getElementsByClassName('card-body').append(p);
+            }
+        })
+        document.getElementById('main').append(list);
+    })
 }
 
-// async function getCards(tableid){
-//     let response = await fetch("https://api.trello.com/1/cards?key=7d05d50b5b8a4569bf25096ffbd2a384&token=bf48aa27fb42789832620a3c57ae6c8025c18bb94d7be86f11ce732f07c7dfa3");
-// }
+async function getLists(tableid){
+    let response = await fetch("https://api.trello.com/1/boards/"+tableid+"/lists?key="+apikey.trim()+"&token="+token.trim());
+    let data = await response.json();
+    lists = data;
+    console.log(lists);
+}
+
+async function getCards(tableid){
+    let response = await fetch("https://api.trello.com/1/boards/"+tableid+"/cards?key="+apikey.trim()+"&token="+token.trim());
+    let data = await response.json();
+    cards = data;
+    console.log(cards);
+}
 
 
